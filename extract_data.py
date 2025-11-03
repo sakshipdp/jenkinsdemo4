4b72e9dc8c564a5025ec2c14a22b675542b6057f
sakshipdp <sakshi.63001@gmail.com>
2025-11-03 22:14:29 +0530

    testing1

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd 
 
-requests.get('https://jsonplaceholder.typicode.com/users')
+response = requests.get('https://jsonplaceholder.typicode.com/users')
 data = response.json()
 df = pd.Dataframe(data)
 df = df [["id","name"]]
